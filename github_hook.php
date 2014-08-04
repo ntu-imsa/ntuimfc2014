8ad8cde979098fd8ba4f6c3d4bd44432b43c37f1
Shouko <h999342@gmail.com>
2014-08-05 04:36:24 +0800

    Update Hook

--- github_hook.php
+++ github_hook.php
@@ -14,26 +14,13 @@ function cidr_match($ip, $cidr)
     return false;
 }
 
-function exec_background($cmd) {
-
-// http://php.net/manual/en/function.exec.php#86329
-// This will run shell command in background
-
-    if (substr(php_uname(), 0, 7) == "Windows"){
-        pclose(popen("start /B ". $cmd, "r"));
-    }
-    else {
-        exec($cmd . " > /dev/null &");
-    }
-}
-
 // Make sure request comes from GitHub hook
 
 if(cidr_match($_SERVER['REMOTE_ADDR'], '192.30.252.0/22')){
-exec_background('git pull');
+	system('git pull');
 }else{
-header('HTTP/1.1 401 Unauthorized');
-echo 'Access Denined';
+	header('HTTP/1.1 401 Unauthorized');
+	echo 'Access Denined';
 }
 
 ?>
